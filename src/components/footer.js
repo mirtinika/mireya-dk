02785ec933dba528594134feeefe45630633cfd0
Mireya Caballero <mireiacr@gmail.com>
2020-10-19 15:13:36 +0200

    Create footer component with StaticQuery

---
+++ src/components/footer.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import {StaticQuery, graphql} from 'gatsby'
+
+import Wrapper from './wrapper'
+import styles from './footer.module.css'
+
+
+export default ({ data }) => (
+  <div className={styles.footer}>
+    <Wrapper>
+    <div>
+      <StaticQuery
+      query={graphql`
+      query FooterQuery {
+        allContentfulFooter {
+          edges {
+            node {
+              buildWith
+            }
+          }
+        }
+      }
+    `
+    }
+      render={
+        data =>{
+        const [footer] = data.allContentfulFooter.edges
+        return(
+          footer.node.buildWith.map((item) => <p>{item}</p>)
+          )
+    }}
+      />
+    </div>
+    </Wrapper>
+  </div>
+)
